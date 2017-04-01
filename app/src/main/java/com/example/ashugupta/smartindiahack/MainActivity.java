1e52993e406d2f6710931261f989e6893d484c6d
ronaksakhuja@gmail.com <ronaksakhuja@gmail.com>
2017-04-01 14:02:45 +0530

    Added touch service

--- app/src/main/java/com/example/ashugupta/smartindiahack/MainActivity.java
+++ app/src/main/java/com/example/ashugupta/smartindiahack/MainActivity.java
@@ -4,18 +4,15 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Handler;
+import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -88,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         Uri gmmIntentUri = Uri.parse("google.navigation:q=" + dest + "&mode=w");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
+        MainActivity.this.startService(new Intent(MainActivity.this,touchservice.class));
         startActivity(mapIntent);
     }
 
